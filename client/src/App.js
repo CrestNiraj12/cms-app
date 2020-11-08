8725c670f4243c61b5282a2047e81e5fbdfbf7cf
CrestNiraj12 <crestniraj@gmail.com>
2020-11-08 23:33:22 +0545

    rough commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import styles from "./App.module.css";
+import NewsCard from "./components/NewsCard";
+import PostCard from "./components/PostCard";
+import { Grid } from "@material-ui/core";
 
 class App extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <h1>Dashboard</h1>
-        <p>Hey</p>
+        <h1>News</h1>
+        <Grid container>
+          <NewsCard />
+        </Grid>
+        <h1 style={{ margin: "40px 0 0 0" }}>Blog Posts</h1>
+        <Grid container justify="flex-start">
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </Grid>
       </div>
     );
   }
---
+++ client/src/components/NewsCard/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Grid,
+} from "@material-ui/core";
+
+import styles from "./NewsCard.module.css";
+
+const NewsCard = () => {
+  return (
+    <Grid
+      item
+      component={Card}
+      xs={12}
+      md={12}
+      className={styles.root}
+      variant="outlined"
+    >
+      <CardContent>
+        <Typography className={styles.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          benevolent
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly. {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Grid>
+  );
+};
+
+export default NewsCard;
